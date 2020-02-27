bc8eaf34d6a870020cd8a4c3fd1ecf24f036dfe7
Roger Capdevila Castells <rogercapdevilacastells@macbook-air-de-roger.local>
2020-02-27 21:26:48 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { BudgetComponent } from './budget/budget.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    BudgetComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
