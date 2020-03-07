f27ccbc9dbd11f78e75ac06ea68becc3f9808f33
rogercapdevilacastells <47901532Roger>
2020-03-07 17:12:08 +0100

    created budget service

--- src/app/components/budgets/budgets.component.ts
+++ src/app/components/budgets/budgets.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Budget } from '../../models/budget';
+import {b}
 
 @Component({
-  selector: 'app-budget',
+  selector: 'app-budgets',
   templateUrl: './budgets.component.html',
   styleUrls: ['./budgets.component.css']
 })
 export class BudgetsComponent implements OnInit {
+  budgets: Budget[];
+  selectedBudget: Budget;
 
-  constructor() { }
+  constructor(private budgetService: BudgetService) { }
 
   ngOnInit(): void {
+    this.getBudgets()
+  }
+
+  onSelect(budget: Budget): void {
+    this.selectedBudget = budget;
+}
+
+  getBudgets(): void {
+    this.budgets = this.budgetService.getBudgets();
   }
 
 }
