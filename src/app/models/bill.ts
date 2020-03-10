db91349cd424c70cb866ef3be6b24804282a2e0c
Roger <rogercapcas@gmail.com>
2020-03-10 20:55:24 +0100

    all interfaces to classes

--- src/app/models/bill.ts
+++ src/app/models/bill.ts
@@ -8,18 +8,20 @@ export class Bill {
   from: Organization;
   to: Organization;
   concept: string;
+  budgetConcept: string;
   totalBT: number; /*Total Before Taxes*/
   taxes: number;
   income: boolean;
   comments: string;
 
-  constructor(billNumber: number, edition: Edition, date: Date, from: Organization, to: Organization, concept: string, totalBT: number, taxes: number, income: boolean, comments: string) {
+  constructor(billNumber: number, edition: Edition, date: Date, from: Organization, to: Organization, concept: string, budgetConcept: string, totalBT: number, taxes: number, income: boolean, comments: string) {
     this.billNumber = billNumber;
     this.edition = edition;
     this.date = date;
     this.from = from;
     this.to = to;
     this.concept = concept;
+    this.budgetConcept = budgetConcept;
     this.totalBT = totalBT;
     this.taxes = taxes;
     this.income = income;
--- src/app/models/other_in_out_comes.ts
+++ src/app/models/other_in_out_comes.ts
@@ -1,14 +1,30 @@
 import { Edition } from './edition';
 import { Organization } from './organization';
 
-export interface Otherinoutcomes {
+export class Otherinoutcomes {
   edition: Edition;
   type: string;
   date: Date;
   from: Organization;
   to: Organization;
   concept: string;
-  total_bt: number; /*Total Before Taxes*/
+  budgetConcept: string;
+  totalBT: number; /*Total Before Taxes*/
   taxes: number;
-  outcome: boolean;
+  income: boolean;
+  comments: string;
+
+  constructor(edition: Edition, type: string, date: Date, from: Organization, to: Organization, concept: string, budgetConcept:string, totalBT: number, taxes: number, income: boolean, comments: string) {
+    this.edition = edition;
+    this.type = type;
+    this.date = date;
+    this.from = from;
+    this.to = to;
+    this.concept = concept;
+    this.budgetConcept = budgetConcept;
+    this.totalBT = totalBT;
+    this.taxes = taxes;
+    this.income = income;
+    this.comments = comments;
+  }
 }
