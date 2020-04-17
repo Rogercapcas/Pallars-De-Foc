c4cb605d2d1cdea6bcb44259bed905a4cc62fc8b
Roger <rogercapcas@gmail.com>
2020-04-17 18:06:22 +0200

    new budget finished, and made all bill atributes public

--- src/app/models/bill.ts
+++ src/app/models/bill.ts
@@ -2,17 +2,17 @@ import { Organization } from './organization';
 import { Edition } from './edition';
 
 export class Bill {
-  billNumber: number;
-  edition: Edition;
-  date: Date;
-  from: Organization;
-  to: Organization;
-  concept: string;
-  budgetConcept: string;
-  totalBT: number; /*Total Before Taxes*/
-  taxes: number;
-  income: boolean; /* if its value is true means income, when its value is false means outcome*/
-  comments: string;
+  public billNumber: number;
+  public edition: Edition;
+  public date: Date;
+  public from: Organization;
+  public to: Organization;
+  public concept: string;
+  public budgetConcept: string;
+  public totalBT: number; /*Total Before Taxes*/
+  public taxes: number;
+  public income: boolean; /* if its value is true means income, when its value is false means outcome*/
+  public comments?: string;
 
   constructor(billNumber: number, edition: Edition, date: Date, from: Organization, to: Organization, concept: string, budgetConcept: string, totalBT: number, taxes: number, income: boolean, comments: string) {
     this.billNumber = billNumber;
--- src/app/models/budget.ts
+++ src/app/models/budget.ts
@@ -1,19 +1,83 @@
 import { Edition } from './edition';
+import {getLocaleDateFormat} from '@angular/common';
 
 export class Budget {
-  name: string;
-  edition: Edition;
-  concepts: [boolean, string, number][]; /* [income, concept, ammount in €] */
-  totalAmmount: number;
-  comments: string;
-  aproved: boolean;
+  public edition: Edition;
+  public totalAmount = 0.0;
+  public creationDate: Date;
+  // INCOMES
+  // Group 1: public founds (pf)
+  public pfGrants: number;
+  public pfBills: number;
+  public pfOthers: number;
 
-  constructor(name: string, edition: Edition, concepts: [boolean, string, number][], totalAmmount: number, comments: string, aproved: boolean ) {
-    this.name = name;
+  // Group 2: private contributions (pc)
+  public pcCapital: number;
+  public pcKind: number; // en espècies
+  public pcOthers: number;
+
+  // Group 3: sells (s)
+  public sBar: number;
+  public sMerchandising: number;
+  public sTicketsQuotes: number;
+  public sOthers: number;
+
+  // Group 4: own funds
+  public ownFunds: number;
+
+  // OUTCOMES
+  // Group 5: difusió (d)
+  public dPosters: number;
+  public dDesign: number;
+  public dDigitalMedia: number;
+  public dTraditionalMedia: number;
+  public dMerchandising: number;
+  public dGiftsSouvenirsPrizes: number;
+  public dWeb: number;
+  public dOthers: number;
+
+  // Group 6: Compensations (c)
+  public cGivenServices: number;
+  public cGoods: number;
+  public cFoodDrinkSleep: number;
+  public cTransport: number;
+  public cOthers: number;
+
+  // Group 7: Rentals (r)
+  public rInfraestructures: number;
+  public rForniture: number;
+  public rOthers: number;
+
+  // Group 8: Hiring (h) ==> Contractacions
+  public hArtists: number;
+  public hServices: number;
+  public hLightSoundSystem: number;
+  public hOthers: number;
+
+  // Group 9: Bar service (b)
+  public bGoods: number; // Tot el que sigui consumible
+  public bGlasses: number; // gots
+  public bForniture: number;
+  public bTickets: number;
+  public bOthers: number;
+
+  // Group 10: Main activity (ma)
+  public maGoods: number; // Piro, pólvora, etc...
+  public maInfraestructure: number; // Masses, paraigües, etc...
+  public maEnsurance: number;
+  public maFungible: number;
+  public maOthers: number;
+
+  // Group 11: Others (o)
+  public oEnsurance: number;
+  public oFungible: number;
+  public oSponsorsMarketing: number; // cartells patrocinadors
+  public oAccreditationsAndStuff: number; // Acreditacions, samarretes, liners, etc... per la organització i convidats
+
+  public comments?: string;
+
+  constructor(edition: Edition, date: Date) {
     this.edition = edition;
-    this.concepts = concepts;
-    this.totalAmmount = totalAmmount;
-    this.comments = comments;
-    this.aproved = aproved;
+    this.creationDate = date;
   }
 }
